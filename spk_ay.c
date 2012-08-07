d8a5c34cc55c5d6023e756f59a21b998dcca5748
Sergio Costas <raster@rastersoft.com>
2012-08-08 00:10:34 +0200

    Changed version number and adjusted copyright notes
    Added VERSION notes

--- spk_ay.c
+++ spk_ay.c
@@ -1,14 +1,14 @@
 /* 
  * Copyright (C) 2012 Fabio Olimpieri
  * Copyright 2003-2009 (C) Raster Software Vigo (Sergio Costas)
- * This file is part of FBZX Wii
+ * This file is part of FBZX and FBZX Wii
  *
- * FBZX Wii is free software; you can redistribute it and/or modify
+ * FBZX (qWii) is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * FBZX Wii is distributed in the hope that it will be useful,
+ * FBZX (Wii) is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
